11158e926f4f76e6f6350f5f743fe6c138dc0319
Praneeth <malpraneeth@deloitte.com>
2022-03-23 13:26:20 +0530

    Miniassignment1(2)

---
+++ MiniAssignment2.java
@@ -0,0 +1,11 @@
+package Mini_Assignment;
+
+public class MiniAssignment2 {
+    /** By checkin = By.xpath("//input[@id='checkin']");
+     By checkout = By.xpath("//input[@id='checkout']");
+     By minusAdults = By.xpath("//i[contains(@class,'minus')]//adult::div[contains(@class,'qtyDec')]");
+     By plusAdults = By.xpath("//i[contains(@class,'plus')]//adult::div[contains(@class,'qtyDec')]");
+     By minusChilds = By.xpath("//i[contains(@class,'minus')]//parent::div[contains(@class,'qtyDec')]");
+     By plusChilds = By.xpath("//i[contains(@class,'plus')]//parent::div[contains(@class,'qtyDec')]");
+     **/
+}   
